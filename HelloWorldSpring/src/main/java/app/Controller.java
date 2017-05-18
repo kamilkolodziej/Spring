db949cb5e1553fa048b0434d5c64cd876e7eeed6
Kamil <k.kolodziej87@gmail.com>
2017-05-18 14:25:20 +0200

    add helloWorldSpring

---
+++ HelloWorldSpring/src/main/java/app/Controller.java
@@ -0,0 +1,14 @@
+package app;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	
+	@RequestMapping("/hello") 
+	public String index() {
+		return "Hello World in Spring Boot.";
+	}
+
+}
